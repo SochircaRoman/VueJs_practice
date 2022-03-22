46bd6ede5349142eef9f0bc7e46e2513741a4794
User01 <romansochirca30@gmail.com>
2022-03-22 14:12:34 +0200

    add ex03

---
+++ ex03/app.js
@@ -0,0 +1,55 @@
+Vue.createApp({
+    data() {
+        return {
+            message: 'Hello from Vue!',
+            name: "",
+            x: 4,
+            y: 2,
+            students: [
+                {
+                    name: "Mihai",
+                    age: 12,
+                    salary: 1234
+                },
+                {
+                    name: "Ion",
+                    age: 33,
+                    salary: 3333
+                }
+            ],
+            isWeekend: true,
+            number: 0,
+            appBackgroundColor: "white"
+        }
+    },
+    watch: {
+        appBackgroundColor() {
+            alert(`Color changed to ${this.appBackgroundColor}`)
+        }
+    },
+    computed: {
+        sum() {
+            return this.x + this.y;
+        },
+        totalSalary() {
+            return this.students.reduce((sum, student) => sum + student.salary, 0)
+        },
+        reversedName() {
+            return this.name.split("").reverse().join("");
+        }
+    },
+    methods: {
+        changeBackgroudColorToBlack() {
+            this.appBackgroundColor = "black";
+        },
+        changeBackgroudColorToGreen() {
+            this.appBackgroundColor = "green";
+        },
+        changeBackgroudColorToBlue() {
+            this.appBackgroundColor = "blue";
+        },
+        addNewItem() {
+
+        }
+    }
+}).mount('#app')

cae3d130265d0fa97e8c514fa5ab84efd40bf44c
User01 <romansochirca30@gmail.com>
2022-03-27 10:25:20 +0300

    Add Vue project_01

---
+++ project_01/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Post from '@/components/Post.vue'
+</script>
+
+<template>
+  <Post/>
+</template>
+
+<style>
+
+</style>
---
+++ project_01/src/components/Post.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data: () =>  {
+    return {
+      posts: [
+        {id: 1, title: "JavaScript 1", body: "JavaScript description"},
+        {id: 2, title: "JavaScript 2", body: "JavaScript description"},
+        {id: 3, title: "JavaScript 3", body: "JavaScript description"},
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="post" v-for="post in posts" :key="photo">
+    <div><strong>{{post.title}}</strong> - {{post.body}}</div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+</style>
